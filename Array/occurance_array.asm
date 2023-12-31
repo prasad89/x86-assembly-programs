83d01dcb2e9cf87a1e6c99f10665d0fba7271e66
prasad89 <vdbhaleraovb@gmail.com>
2023-12-31 14:20:38 +0530

    Array

--- Array/occurance_array.asm
+++ Array/occurance_array.asm
@@ -1,30 +1,36 @@
 section .data
-	Array dd 40,10,20,30,40,10,10,40,50,-1
-	msg db "40 Occured %d times",10,0
+    Array dd 40, 10, 20, 30, 40, 10, 10, 40, 50, -1
+    msg db "40 Occurred %d times", 10, 0
+
 section .text
-	global main
-	extern printf
+    global main
+    extern printf
+
 main:
-	xor ecx,ecx
-	xor esi,esi
+    xor ecx, ecx
+    xor esi, esi
+
 lp:
-	mov ebx, Array
-	mov eax,4
-	mul ecx
-	add ebx,eax
-	cmp dword[ebx],-1
-	jz endof
-	inc ecx
-	cmp dword[ebx],40
-	jz lp2
-	jmp lp
+    mov ebx, Array
+    mov eax, 4
+    mul ecx
+    add ebx, eax
+    cmp dword [ebx], -1
+    jz endof
+    inc ecx
+    cmp dword [ebx], 40
+    jz lp2
+    jmp lp
+
 lp2:
-	inc esi
-	jmp lp
+    inc esi
+    jmp lp
+
 endof:
-	push esi
-	push msg
-	call printf
-	add esp,8
-	
-	ret
+    push esi
+    push msg
+    call printf
+    add esp, 8
+
+terminate:
+    ret
--- Array/scan_array.asm
+++ Array/scan_array.asm
@@ -1,61 +1,56 @@
 section .data
-	msg db "Enter number of Elements: ",10,0
-	input db "%d",0
-	msg2 db "Element of array is: %d",10,0
+    msg db "Enter number of Elements: ", 10, 0
+    input db "%d", 0
+    msg2 db "Element of array is: %d", 10, 0
 
 section .bss
-	n resd 1
-	Array resd 10
+    n resd 1
+    Array resd 10
 
 section .text
-	global main
-	extern printf,scanf
+    global main
+    extern printf, scanf
 
 main:
-	push msg
-	call printf
-	add esp,4
+    push msg
+    call printf
+    add esp, 4
+    push n
+    push input
+    call scanf
+    add esp, 8
+    xor ecx, ecx
 
-	push n
-	push input
-	call scanf
-	add esp,8
-
-	xor ecx,ecx
 scan:
-	mov ebx,Array
-	mov eax,4
-	mul ecx
-	add ebx,eax
+    mov ebx, Array
+    mov eax, 4
+    mul ecx
+    add ebx, eax
+    pusha
+    push ebx
+    push input
+    call scanf
+    add esp, 8
+    popa
+    inc ecx
+    cmp ecx, dword [n]
+    jl scan
+    xor ecx, ecx
 
-	pusha
-	push ebx
-	push input
-	call scanf
-	add esp,8
-	popa
-
-	inc ecx
-	cmp ecx,dword[n]
-	jl scan
-
-	xor ecx,ecx
 print:
-	mov ebx,Array
-	mov eax,4
-	mul ecx
-	add ebx,eax
+    mov ebx, Array
+    mov eax, 4
+    mul ecx
+    add ebx, eax
+    pusha
+    push dword [ebx]
+    push msg2
+    call printf
+    add esp, 8
+    popa
+    inc ecx
+    cmp ecx, dword [n]
+    jl print
 
-	pusha
-	push dword[ebx]
-	push msg2
-	call printf
-	add esp,8
-	popa
-
-	inc ecx
-	cmp ecx,dword[n]
-	jl print
-
-endof:
-	ret
+terminate:
+    ret
