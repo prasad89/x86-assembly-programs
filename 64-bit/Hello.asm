8cbe46269149506c0e6fe16724fd39ef6b7dbc1f
prasad89 <vdbhaleraovb@gmail.com>
2023-12-31 14:07:27 +0530

    64-bit

---
+++ 64-bit/Hello.asm
@@ -0,0 +1,19 @@
+section .data
+    msg db "Hello %d-bit...", 10, 0
+
+section .text
+    global main
+    extern printf
+
+main:
+    xor rax, rax
+    mov rdi, msg
+    mov rsi, 32
+    mov r8, 32
+    add rsi, r8
+    push rdi
+    call printf wrt ..plt
+    pop rdi
+
+terminate:
+    ret
