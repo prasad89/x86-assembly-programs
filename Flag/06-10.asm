3a2098d14859c9f8b91ff8c87c9dd7404b10a38a
prasad89 <vdbhaleraovb@gmail.com>
2023-12-31 14:29:03 +0530

    Flag

--- Flag/06-10.asm
+++ Flag/06-10.asm
@@ -1,41 +1,39 @@
 section .data
-	arr dd 10,30,1,100,50,20,40
-	max db "Max:%d",10,0
-	min db "Min %d",10,0
+    arr dd 10, 30, 1, 100, 50, 20, 40
+    max db "Max:%d", 10, 0
+    min db "Min %d", 10, 0
+
 section .text
-	global main
-	extern printf
+    global main
+    extern printf
+
 main:
-	xor ecx,ecx
-	mov esi,dword[arr]
-	mov edi,dword[arr]
+    xor ecx, ecx
+    mov esi, dword[arr]
+    mov edi, dword[arr]
+
 lp:
-	mov ebx,arr
-	mov eax,4
-	mul ecx
-	add ebx,eax
-	
-	;max
-	cmp esi,dword[ebx]
-	cmovl esi,dword[ebx]
-	
-	;min
-	cmp edi,dword[ebx]
-	cmovg edi,dword[ebx]
-	
-	inc ecx
-	cmp ecx,6
-	
-	jl lp
-	
-	push esi
-	push max
-	call printf
-	add esp,8
-	
-	push edi
-	push min
-	call printf
-	add esp,8
+    mov ebx, arr
+    mov eax, 4
+    mul ecx
+    add ebx, eax
+    cmp esi, dword[ebx] ;; max
+    cmovl esi, dword[ebx]
+    cmp edi, dword[ebx] ;; min
+    cmovg edi, dword[ebx]
+    inc ecx
+    cmp ecx, 6
+    jl lp
+
 endof:
-	ret
+    push esi
+    push max
+    call printf
+    add esp, 8
+    push edi
+    push min
+    call printf
+    add esp, 8
+
+terminate:
+    ret
