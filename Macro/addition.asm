d6d081a6ef66a5b0277baca253adc62da3c5fdff
prasad89 <vdbhaleraovb@gmail.com>
2024-01-01 21:36:33 +0530

    Refactor macro code and fix formatting

--- Macro/addition.asm
+++ Macro/addition.asm
@@ -1,26 +1,25 @@
 %macro addition 3
-mov eax,%1
-mov ebx,%2
-add eax,ebx
-mov %3,eax
+    mov eax, %1
+    mov ebx, %2
+    add eax, ebx
+    mov %3, eax
 %endmacro
 
 section .data
-	op db "Addition of numbers is: %d",10,0
-		
+    op db "Addition of numbers is:%d", 10, 0
+
 section .text
-	global main
-	extern printf
-	
+    global main
+    extern printf
+
 main:
-	addition 3,4,ecx
-	
-	pusha
-	push ecx
-	push op
-	call printf
-	add esp,8
-	popa
-	
+    addition 3, 4, ecx
+    pusha
+    push ecx
+    push op
+    call printf
+    add esp, 8
+    popa
+
 terminate:
-	ret
+    ret
--- Macro/copy_instruction_string.asm
+++ Macro/copy_instruction_string.asm
@@ -1,27 +1,26 @@
 %macro copy 3
-	mov ecx,%1
-	mov esi,%2
-	mov edi,%3
-	rep movsb
+    mov ecx, %1
+    mov esi, %2
+    mov edi, %3
+    rep movsb
 %endmacro
 
 section .data
-	src db "Dummy Text",0
-	len equ $-src
+    src db "Dummy Text", 0
+    len equ $ - src
 
 section .bss
-	dest resb len
+    dest resb len
 
 section .text
-	global main
-	extern puts
-	
+    global main
+    extern puts
+
 main:
-	copy len,src,dest
-	
-	push dest
-	call puts
-	add esp,4
-	
+    copy len, src, dest
+    push dest
+    call puts
+    add esp, 4
+
 terminate:
-	ret
+    ret
